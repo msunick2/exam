4f00a720ec4eaed7bb5342bc4adb4f748ebc7604
Nick Shipilov <msunick@mail.ru>
2023-04-24 21:16:33 +0300

    count chars in array

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string[] makeArrayWithThreeDigits(string[] array) {
+    int count = 0;
+    for(int i = 0; i < array.Length; i++) {
+       if (array[i].Length <= 3) {
+            count++;
+       }
