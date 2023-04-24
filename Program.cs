25d88117edf0948919e93ccd0b429bbcf63aa982
Nick Shipilov <msunick@mail.ru>
2023-04-24 21:23:20 +0300

    make new array and push strings with 3 chars

--- Program.cs
+++ Program.cs
@@ -4,3 +4,15 @@
        if (array[i].Length <= 3) {
             count++;
        }
+    }
+ }
+    string[] newArray = new string[count];
+    if (count > 0) {
+        int j = 0;
+        for(int i = 0; i < array.Length; i++) {
+            if (array[i].Length <= 3) {
+                newArray[j] = array[i];
+                j++;
+            }
+        }   
+    }
